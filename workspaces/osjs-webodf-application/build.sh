c256c7c896bc0340cc004589137bd34ef681f5d1
Frank Lemanschik <frank@lemanschik.com>
2022-11-01 03:06:14 +0100

    git subrepo clone https://github.com/os-js/osjs-webodf-application workspaces/osjs-webodf-application
    subrepo:
      subdir:   "workspaces/osjs-webodf-application"
      merged:   "64c348d8"
    upstream:
      origin:   "https://github.com/os-js/osjs-webodf-application"
      branch:   "master"
      commit:   "64c348d8"
    git-subrepo:
      version:  "0a31831"
      origin:   "git@github.com:stealify/git-subrepo.git"
      commit:   "0a31831"

---
+++ workspaces/osjs-webodf-application/build.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+set -e
+
+npm run eslint
+#npm run stylelint
+
+mkdir -p dist
+
+rm -rf dist/*
+rm -rf build
+
+git submodule init
+git submodule update --remote
+
+mkdir -p build
+pushd build
+  cmake ../WebODF
+  make webodf.js-target
+popd
+
+NODE_ENV=production npm run build
