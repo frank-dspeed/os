d74f1a1d86cac3c1d7626e25d6a7fd521e238490
Frank Lemanschik <frank@lemanschik.com>
2022-11-01 03:06:27 +0100

    git subrepo clone https://github.com/os-js/osjs-example-provider workspaces/osjs-example-provider
    subrepo:
      subdir:   "workspaces/osjs-example-provider"
      merged:   "bb37fca9"
    upstream:
      origin:   "https://github.com/os-js/osjs-example-provider"
      branch:   "master"
      commit:   "bb37fca9"
    git-subrepo:
      version:  "0a31831"
      origin:   "git@github.com:stealify/git-subrepo.git"
      commit:   "0a31831"

---
+++ workspaces/osjs-example-provider/README.md
@@ -0,0 +1,61 @@
+<p align="center">
+  <img alt="OS.js Logo" src="https://raw.githubusercontent.com/os-js/gfx/master/logo-big.png" />
+</p>
+
+[OS.js](https://www.os-js.org/) is an [open-source](https://raw.githubusercontent.com/os-js/OS.js/master/LICENSE) web desktop platform with a window manager, application APIs, GUI toolkit, filesystem abstractions and much more.
+
+[![Support](https://img.shields.io/badge/patreon-support-orange.svg)](https://www.patreon.com/user?u=2978551&ty=h&u=2978551)
+[![Support](https://img.shields.io/badge/opencollective-donate-red.svg)](https://opencollective.com/osjs)
+[![Donate](https://img.shields.io/badge/liberapay-donate-yellowgreen.svg)](https://liberapay.com/os-js/)
+[![Donate](https://img.shields.io/badge/paypal-donate-yellow.svg)](https://paypal.me/andersevenrud)
+[![Community](https://img.shields.io/badge/join-community-green.svg)](https://community.os-js.org/)
+
+# OS.js Example Service Provider
+
+This is the Example Service Provdider for OS.js
+
+## Installation
+
+```
+npm install @osjs/example-provider
+```
+
+## Usage
+
+In you startup procedure, simply import and register:
+
+> Note: This example is written using ESM. Node does not support this out of the box, so assume this is a client scipt.
+
+```
+import {ExampleServiceProvider} from '@osjs/example-provider'; // You want your package name here
+
+// ...
+
+osjs.register(ExampleServiceProvider)
+
+// ...
+
+```
+
+Then you can use `core.make('example/instance')` or `core.make('example/singleton')` (as provided by this example).
+
+## Contribution
+
+* **Sponsor on [Github](https://github.com/sponsors/andersevenrud)**
+* **Become a [Patreon](https://www.patreon.com/user?u=2978551&ty=h&u=2978551)**
+* **Support on [Open Collective](https://opencollective.com/osjs)**
+* [Contribution Guide](https://github.com/os-js/OS.js/blob/master/CONTRIBUTING.md)
+
+## Documentation
+
+See the [Official Manuals](https://manual.os-js.org/) for articles, tutorials and guides.
+
+## Links
+
+* [Official Chat](https://gitter.im/os-js/OS.js)
+* [Community Forums and Announcements](https://community.os-js.org/)
+* [Homepage](https://os-js.org/)
+* [Twitter](https://twitter.com/osjsorg) ([author](https://twitter.com/andersevenrud))
+* [Google+](https://plus.google.com/b/113399210633478618934/113399210633478618934)
+* [Facebook](https://www.facebook.com/os.js.org)
+* [Docker Hub](https://hub.docker.com/u/osjs/)
---
+++ workspaces/osjs-example-provider/src/provider.js
@@ -0,0 +1,79 @@
+/*
+ * OS.js - JavaScript Cloud/Web Desktop Platform
+ *
+ * Copyright (c) Anders Evenrud <andersevenrud@gmail.com>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @author  Anders Evenrud <andersevenrud@gmail.com>
+ * @licence Simplified BSD License
+ */
+
+class InstanceExample {
+  greet() {
+    return 'Instance Example';
+  }
+}
+
+class SingletonExample {
+  greet() {
+    return 'Singleton Example';
+  }
+}
+
+/**
+ * Example OS.js Service Provider
+ */
+export default class ExampleServiceProvider {
+
+  constructor(core, options = {}) {
+    this.core = core;
+    this.options = options;
+  }
+
+  /* The list of registered services */
+  provides() {
+    return [
+      'example/singleton',
+      'example/instance'
+    ];
+  }
+
+  /* Initialize your services */
+  async init() {
+    this.core.singleton('example/singleton', () => new SingletonExample());
+    this.core.instance('example/instance', () => new InstanceExample());
+
+    /*
+    Example usage:
+    core.make('example/singleton')
+    core.make('example/instance')
+    */
+  }
+
+  /* Start your services */
+  start() {}
+
+  /* Clean up */
+  destroy() {}
+
+}
