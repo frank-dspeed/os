55f574ad9ab6908912084372664484789d4cad60
Frank Lemanschik <frank@lemanschik.com>
2022-11-01 03:06:03 +0100

    git subrepo clone https://github.com/os-js/osjs-settings-application workspaces/osjs-settings-application
    subrepo:
      subdir:   "workspaces/osjs-settings-application"
      merged:   "c347b1dd"
    upstream:
      origin:   "https://github.com/os-js/osjs-settings-application"
      branch:   "master"
      commit:   "c347b1dd"
    git-subrepo:
      version:  "0a31831"
      origin:   "git@github.com:stealify/git-subrepo.git"
      commit:   "0a31831"

---
+++ workspaces/osjs-settings-application/webpack.config.js
@@ -0,0 +1,65 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+
+const mode = process.env.NODE_ENV || 'development';
+const minimize = mode === 'production';
+const plugins = [];
+
+if (mode === 'production') {
+  plugins.push(new OptimizeCSSAssetsPlugin({
+    cssProcessorOptions: {
+      discardComments: true
+    },
+  }));
+}
+
+module.exports = {
+  mode,
+  devtool: 'source-map',
+  entry: [
+    path.resolve(__dirname, 'index.js'),
+  ],
+  optimization: {
+    minimize,
+  },
+  externals: {
+    osjs: 'OSjs'
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css'
+    }),
+    ...plugins
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true
+            }
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true
+            }
+          }
+        ]
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules\/(?!@osjs)/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  }
+};
