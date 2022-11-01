9a2b0ba1b9a99980ce983402cd95533ffc936769
Frank Lemanschik <frank@lemanschik.com>
2022-11-01 03:07:38 +0100

    git subrepo clone https://github.com/os-js/osjs-example-application workspaces/osjs-example-application
    subrepo:
      subdir:   "workspaces/osjs-example-application"
      merged:   "2a4df6ec"
    upstream:
      origin:   "https://github.com/os-js/osjs-example-application"
      branch:   "master"
      commit:   "2a4df6ec"
    git-subrepo:
      version:  "0a31831"
      origin:   "git@github.com:stealify/git-subrepo.git"
      commit:   "0a31831"

---
+++ workspaces/osjs-example-application/index.js
@@ -0,0 +1,68 @@
+/*
+ * OS.js - JavaScript Cloud/Web Desktop Platform
+ *
+ * Copyright (c) Anders Evenrud <andersevenrud@gmail.com>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @author  Anders Evenrud <andersevenrud@gmail.com>
+ * @licence Simplified BSD License
+ */
+
+import './index.scss';
+import osjs from 'osjs';
+import {name as applicationName} from './metadata.json';
+
+// Our launcher
+const register = (core, args, options, metadata) => {
+  // Create a new Application instance
+  const proc = core.make('osjs/application', {args, options, metadata});
+
+  // Create  a new Window instance
+  proc.createWindow({
+    id: 'MyApplicationWindow',
+    title: metadata.title.en_EN,
+    icon: proc.resource(proc.metadata.icon),
+    dimension: {width: 400, height: 400},
+    position: {left: 700, top: 200}
+  })
+    .on('destroy', () => proc.destroy())
+    .render();
+
+  // Creates a new WebSocket connection (see server.js)
+  //const sock = proc.socket('/socket');
+  //sock.on('message', (...args) => console.log(args))
+  //sock.on('open', () => sock.send('Ping'));
+
+  // Use the internally core bound websocket
+  //proc.on('ws:message', (...args) => console.log(args))
+  //proc.send('Ping')
+
+  // Creates a HTTP call (see server.js)
+  //proc.request('/test', {method: 'post'})
+  //.then(response => console.log(response));
+
+  return proc;
+};
+
+// Creates the internal callback function when OS.js launches an application
+osjs.register(applicationName, register);
